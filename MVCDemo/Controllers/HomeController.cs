70a87f2e940acd9551181700f88b3427e7de0312
adarshk <adarshk@ebsco.com>
2016-02-19 00:35:00 -0800

    Initial Draft

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCDemo.IISServiceReference;
+
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+	        using (var client = new CalculatorServiceClient())
+	        {
+		        return client.Add(3, 5).ToString();
+
+	        }
+	        return "Error";
+        }
+    }
+}
